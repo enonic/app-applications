89ce29942e2eded6317a60a0e42dd71f3b4b9cdb
Alan Semenov <ase@enonic.com>
2021-01-20 09:36:14 +0100

    Removed deprecated config of Terser plugin

--- webpack.config.js
+++ webpack.config.js
@@ -44,7 +44,6 @@ module.exports = {
     optimization: {
         minimizer: [
             new TerserPlugin({
-                sourceMap: true,
                 terserOptions: {
                     compress: {
                         drop_console: false
