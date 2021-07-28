d486a63132aee0a071c16ce8435eadad4acbe111
Mikita Taukachou <edloidas@gmail.com>
2021-07-28 15:46:34 +0300

    Update lib-admin-ui sourcemaps processing #415
    Added loader to process the 3rd party sourcemaps properly, making them visible in the browser.
    Changed `source-map` to `eval-source-map` for faster rebuilds.

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CircularDependencyPlugin = require('circular-dependency-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const ProvidePlugin = require('webpack/lib/ProvidePlugin');
-const ErrorLoggerPlugin = require('error-logger-webpack-plugin');
 const path = require('path');
 
 const isProd = process.env.NODE_ENV === 'production';
@@ -22,6 +21,14 @@ module.exports = {
     },
     module: {
         rules: [
+            {
+                test: /\.js$/,
+                enforce: 'pre',
+                use: ['source-map-loader'],
+                exclude: [
+                    path.resolve(__dirname, 'node_modules/fine-uploader/'),
+                ],
+            },
             {
                 test: /\.tsx?$/,
                 use: [{loader: 'ts-loader', options: {configFile: 'tsconfig.build.json'}}]
@@ -68,9 +75,8 @@ module.exports = {
             exclude: /a\.js|node_modules/,
             failOnError: true
         }),
-        //new ErrorLoggerPlugin({showColumn: false})
     ],
     mode: isProd ? 'production' : 'development',
-    devtool: isProd ? false : 'source-map',
-    performance: { hints: false }
+    devtool: isProd ? false : 'eval-source-map',
+    performance: {hints: false}
 };
