246070e3ff30c2906407051f5579197660249150
Alan Semenov <ase@enonic.com>
2022-02-19 17:17:47 +0100

    Fixed marketUrl config not being used. Turned on Content Security Policy (#643)

--- src/main/resources/admin/tools/main/main.js
+++ src/main/resources/admin/tools/main/main.js
@@ -3,6 +3,11 @@ const mustache = require('/lib/mustache');
 const portal = require('/lib/xp/portal');
 const i18n = require('/lib/xp/i18n');
 
+function getMarketUrl() {
+    const marketConfigBean = __.newBean('com.enonic.xp.app.main.GetMarketConfigBean');
+    return __.toNativeObject(marketConfigBean.getMarketUrl());
+}
+
 function handleGet() {
     const view = resolve('./main.html');
 
@@ -19,9 +24,16 @@ function handleGet() {
         configServiceUrl: portal.serviceUrl({service: 'config'})
     };
 
+    const marketUrl = getMarketUrl();
+    const baseMarketUrl = marketUrl.substring(0, marketUrl.indexOf('/', 9));
+    const marketSrc = ' \'self\' ' + baseMarketUrl + ';';
+
     return {
         contentType: 'text/html',
-        body: mustache.render(view, params)
+        body: mustache.render(view, params),
+        headers: {
+            'Content-Security-Policy': 'default-src \'self\'; script-src \'self\' \'unsafe-eval\'; style-src \'self\' \'unsafe-inline\'; object-src \'none\'; connect-src ' + marketSrc + ' img-src ' + marketSrc
+        }
     };
 }
 
--- src/main/resources/assets/js/app/resource/MarketApplicationFetcher.ts
+++ src/main/resources/assets/js/app/resource/MarketApplicationFetcher.ts
@@ -1,14 +1,16 @@
 import * as Q from 'q';
 import {MarketApplicationResponse} from 'lib-admin-ui/application/MarketApplicationResponse';
 import {ListMarketApplicationsRequest} from 'lib-admin-ui/application/ListMarketApplicationsRequest';
+import {CONFIG} from 'lib-admin-ui/util/Config';
 
 export class MarketApplicationFetcher {
 
-    static fetchApps(version: string, from: number = 0, size: number = -1): Q.Promise<MarketApplicationResponse> {
+    static fetchApps(from: number = 0, size: number = -1): Q.Promise<MarketApplicationResponse> {
         return new ListMarketApplicationsRequest()
+            .setUrl(CONFIG.getString('marketUrl'))
             .setStart(from)
             .setCount(size)
-            .setVersion(version)
+            .setVersion(CONFIG.getString('xpVersion'))
             .sendAndParse();
     }
 }
