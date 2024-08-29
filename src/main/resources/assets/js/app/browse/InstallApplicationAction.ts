70c95e38653b663cbf47345761a1ec4659ac7300
ashklianko <asl@enonic.com>
2024-08-29 10:59:39 +0200

    ApplicationTreeGrid: replace Slickgrid with ListBox #1739

--- src/main/resources/assets/js/app/browse/InstallApplicationAction.ts
+++ src/main/resources/assets/js/app/browse/InstallApplicationAction.ts
@@ -1,17 +1,17 @@
-import {ApplicationTreeGrid} from './ApplicationTreeGrid';
 import {InstallAppPromptEvent} from '../installation/InstallAppPromptEvent';
 import {i18n} from '@enonic/lib-admin-ui/util/Messages';
 import {Application} from '@enonic/lib-admin-ui/application/Application';
 import {Action} from '@enonic/lib-admin-ui/ui/Action';
+import {SelectableListBoxWrapper} from '@enonic/lib-admin-ui/ui/selector/list/SelectableListBoxWrapper';
 
 export class InstallApplicationAction
     extends Action {
 
-    constructor(applicationTreeGrid: ApplicationTreeGrid) {
+    constructor(selectionWrapper: SelectableListBoxWrapper<Application>) {
         super(i18n('action.install'));
         this.setEnabled(false);
         this.onExecuted(() => {
-            const installedApplications: Application[] = applicationTreeGrid.getDefaultData();
+            const installedApplications: Application[] = selectionWrapper.getSelectedItems();
             new InstallAppPromptEvent(installedApplications).fire();
         });
     }
