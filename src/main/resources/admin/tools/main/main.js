c96d7647b96e6b32ee794b22ee08908e50eeda96
Anatol Sialitski <asi@enonic.com>
2024-10-08 14:18:44 +0200

    Improve configuration and start to use apiUrl function #1771

--- src/main/resources/admin/tools/main/main.js
+++ src/main/resources/admin/tools/main/main.js
@@ -10,6 +10,29 @@ function getMarketUrl() {
     return __.toNativeObject(marketConfigBean.getMarketUrl());
 }
 
+function getConfigAsJson() {
+    const readonlyMode = app.config['readonlyMode'] === 'true' || false;
+
+    return JSON.stringify({
+        adminUrl: admin.getBaseUri(),
+        appId: app.name,
+        assetsUri: portal.assetUrl({path: ''}),
+        marketUrl: getMarketUrl(),
+        readonlyMode: readonlyMode,
+        apis: {
+            i18nUrl: portal.apiUrl({
+                application: app.name,
+                api: 'i18n',
+            }),
+        },
+        toolUri: admin.getToolUrl(
+            app.name,
+            'main'
+        ),
+        xpVersion: app.version,
+    }, null, 4).replace(/<(\/?script|!--)/gi, "\\u003C$1");
+}
+
 function handleGet() {
     const view = resolve('./main.html');
     const toolBaseUrl = admin.getToolUrl(app.name, 'main');
@@ -24,9 +47,10 @@ function handleGet() {
             locale: admin.getLocales()
         }),
         launcherPath: admin.getLauncherPath(),
-        configServiceUrl: `${toolBaseUrl}/_/${app.name}/config`,
         toolBaseUrl: toolBaseUrl,
         toolAppName: app.name,
+        configScriptId: 'app-applications-config-as-json',
+        configAsJson: getConfigAsJson(),
     };
 
     const marketUrl = getMarketUrl();
