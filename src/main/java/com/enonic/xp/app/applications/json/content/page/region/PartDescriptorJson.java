790c13355f55f030aeb9aae60a1eac197d5eb06d
anatol-sialitski <53557255+anatol-sialitski@users.noreply.github.com>
2026-02-17 16:18:21 +0100

    Upgrade to the latest version of XP 8 #2058 (#2059)

--- src/main/java/com/enonic/xp/app/applications/json/content/page/region/PartDescriptorJson.java
+++ src/main/java/com/enonic/xp/app/applications/json/content/page/region/PartDescriptorJson.java
@@ -2,7 +2,7 @@ package com.enonic.xp.app.applications.json.content.page.region;
 
 import com.enonic.xp.app.applications.json.content.page.DescriptorJson;
 import com.enonic.xp.app.applications.rest.resource.schema.content.LocaleMessageResolver;
-import com.enonic.xp.app.applications.rest.resource.schema.mixin.InlineMixinResolver;
+import com.enonic.xp.app.applications.rest.resource.schema.mixin.CmsFormFragmentServiceResolver;
 import com.enonic.xp.region.PartDescriptor;
 
 public class PartDescriptorJson
@@ -10,7 +10,7 @@ public class PartDescriptorJson
 {
 
     public PartDescriptorJson( final PartDescriptor descriptor, final LocaleMessageResolver localeMessageResolver,
-                               final InlineMixinResolver inlineMixinResolver )
+                               final CmsFormFragmentServiceResolver inlineMixinResolver )
     {
         super( descriptor, localeMessageResolver, inlineMixinResolver );
     }
